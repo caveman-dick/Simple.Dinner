a891103e905795e0e53c38f0af9890014ab59422
Ian Battersby <ian.battersby@gmail.com>
2012-07-27 14:17:42 +0100

    Moved to more explicit and TDD-friendly naming convention.

--- src/Simple.Dinner.Web/Properties/AssemblyInfo.cs
+++ src/Simple.Dinner.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple.Dinner")]
+[assembly: AssemblyTitle("Simple.Dinner.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Simple.Dinner")]
+[assembly: AssemblyProduct("Simple.Dinner.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
